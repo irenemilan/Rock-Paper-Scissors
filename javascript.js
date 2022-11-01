f4582cb05ec4d93e8bfece5ec50da1ba76643086
Irene <irenemilan@gmail.com>
2022-11-01 15:03:10 -0400

    Consolidate playRound() and game()

--- javascript.js
+++ javascript.js
@@ -1,35 +1,28 @@
+availableChoices = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    if(choice == 0) return "rock";
-    if(choice == 1) return "paper";
-    if(choice == 2) return "scissors";
-}
-
-function playRound() {
-    let round = 1;
-    let computerSelection = getComputerChoice();
-    let playerSelection = window.prompt(`Round ${round}.\nRock, Paper or Scissors?`).toLowerCase();
-    console.log(`The computer chose ${computerSelection}.`);
-    if(playerSelection == computerSelection) return "It's a tie.";
-    else if(playerSelection == "rock" && computerSelection == "scissors") return "You win! Rock beats Scissors";
-    else if(playerSelection == "paper" && computerSelection == "rock") return "You win! Paper beats Rock";
-    else if(playerSelection == "scissors" && computerSelection == "paper") return "You win! Scissors beats Paper";
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-
+    return Math.floor(Math.random() * 3);
 }
 
 function game(){
-    let comp = 0;
-    let player = 0;
-    let result;
-    while(comp < 5 && player < 5) {
-        console.log(`Current score:\nComp: ${comp}\nPlayer: ${player}`);
-        result = playRound();
-        if(result[4] == 'w') player++;
-        else if(result[4] == 'l') comp++;
-        console.log(result);
+    let compCount = 0;
+    let playerCount = 0;
+    while(compCount < 5 && playerCount < 5) {
+        let comp = getComputerChoice();
+        console.log(`Current score:\nComp: ${compCount}\nPlayer: ${playerCount}`);
+        let player = availableChoices.indexOf(window.prompt('Rock, Paper or Scissors?').toLowerCase());
+        console.log(`The computer chose ${availableChoices[comp]}.`)
+        if(comp == player) console.log("It's a tie.");
+        else if(player == ((comp + 1) % 3)){
+            console.log(`You lose! ${availableChoices[comp]} beats ${availableChoices[player]}.`);
+            compCount++;
+        }
+        else {
+            console.log(`You win! ${availableChoices[player]} beats ${availableChoices[comp]}.`);
+            playerCount++;
+        }
      }
-    return player > comp ? "Congratulations you beat the computer!": "You lost against the computer! Try again!";
+    return playerCount > compCount ? "Congratulations you beat the computer!": "Oh no! The computer won.";
 }
 
 console.log(game());
